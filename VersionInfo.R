d265ffbadca19f8d2b155e3f6774dbcce68ffff6
Dan Dittenhafer <daniel@dittenhafer.net>
2014-08-29 21:44:19 -0400

    Added DMwR sales info

--- VersionInfo.R
+++ VersionInfo.R
@@ -1,7 +1,15 @@
 # 
 # R version 3.1.1 and RStudio Version 0.98.1028
 #
-# PostgreSQL Version: 
+# PostgreSQL Version: *** TBD ***
 #
-# DMwR sales data set: 
-#
+# DMwR sales data set
+# -------------------
+# Load the Data Mining with R package
+library("DMwR")
+
+# How many data points per observation? Ans: 5
+length(DMwR::sales) 
+
+# How many observations? Ans: 401146
+DMwR::sales[0]
