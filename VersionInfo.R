edfefda301e07b1f87eb06c9819ae08305026eca
Dan Dittenhafer <daniel@dittenhafer.net>
2014-08-31 08:05:24 -0400

    rstudio versionInfo + PostgreSQL dbGetInfo

--- VersionInfo.R
+++ VersionInfo.R
@@ -2,9 +2,14 @@
 # R.version.string returns "R version 3.1.1 (2014-07-10)" for me.
 R.version.string
 
-# RStudio Version 0.98.1028
+# rstudio::versionInfo() returns  $version = "0.98.1028" and $mode = "desktop"
+rstudio::versionInfo()
 
 # PostgreSQL Version: 9.3.5.1 *** TBD ***
+require("RPostgreSQL")
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv,user="postgres",password="a",dbname="postgres")
+dbGetInfo(con) # Returns, among other things, $serverVersion = "9.3.5"
 
 # DMwR sales data set
 # -------------------
