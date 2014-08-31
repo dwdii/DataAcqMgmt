7574858d1b1e346bb46d6043048567f4d6ad4d49
Dan Dittenhafer <daniel@dittenhafer.net>
2014-08-31 07:49:06 -0400

    R version + PostgreSQL version

--- VersionInfo.R
+++ VersionInfo.R
@@ -1,8 +1,11 @@
 # 
-# R version 3.1.1 and RStudio Version 0.98.1028
-#
-# PostgreSQL Version: *** TBD ***
-#
+# R.version.string returns "R version 3.1.1 (2014-07-10)" for me.
+R.version.string
+
+# RStudio Version 0.98.1028
+
+# PostgreSQL Version: 9.3.5.1 *** TBD ***
+
 # DMwR sales data set
 # -------------------
 # Load the Data Mining with R package
@@ -13,3 +16,4 @@ length(DMwR::sales)
 
 # How many observations? Ans: 401146
 DMwR::sales[0]
+
