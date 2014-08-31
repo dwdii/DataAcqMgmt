a9851606a6fae84c84eecd93c1863d13d9eb219b
Dan Dittenhafer <daniel@dittenhafer.net>
2014-08-31 19:07:24 -0400

    Initial Week 2

---
+++ Week2-Quiz.R
@@ -0,0 +1,13 @@
+#
+#     Creator: Daniel Dittenhafer
+#
+#     Created: Aug 31, 2014
+#
+# Description: Answers to Week 2 Quiz
+#
+
+# 1. Create a vector that contains 20 numbers.
+myFirstVector <- c(2,4,4,6,8,9,10,12,13,13,15,16,18,19,20,21,21,24,25,26)
+length(myFirstVector) # = 20
+
+# 2. Use R to convert the vector from question 1 into a character vector.
---
+++ Week2.R
@@ -0,0 +1,8 @@
+#
+#     Creator: Daniel Dittenhafer
+#
+#     Created: Aug 31, 2014
+#
+# Description: Answers to Week 2 Assignment - Exercises
+#
+
