6ebee85392b1057626506177faf9ba4b0a7ef525
Dan Dittenhafer <daniel@dittenhafer.net>
2014-09-04 21:20:11 -0400

    Adjusted comments

--- Week2.R
+++ Week2.R
@@ -6,41 +6,58 @@
 # Description: Answers to Week 2 Assignment - Exercises
 #
 
-# 1.a. Assign the five individuals to a vector called queue
+#### 1.a. #### 
+# Assign the five individuals to a vector called queue
 (queue <- c("James", "Mary", "Steve", "Alex", "Patricia"))
+# [1] "James"    "Mary"     "Steve"    "Alex"     "Patricia"
 
-# 1.b. Update the queue for the arrival of a new patron named Harold.
+#### 1.b. ####
+# Update the queue for the arrival of a new patron named Harold.
 (queue <- c(queue, "Harold"))
+# [1] "James"    "Mary"     "Steve"    "Alex"     "Patricia" "Harold" 
 
-# 1.c. Update the queue to reflect the fact that James has finished checking out.
+#### 1.c. ####
+# Update the queue to reflect the fact that James has finished checking out.
 (queue <- queue[2:length(queue)])
+# [1] "Mary"     "Steve"    "Alex"     "Patricia" "Harold" 
 
-# 1.d. Update the queue to reflect the fact that Patricia has talked her way 
-#      in front of Steve with just one item.
+#### 1.d. ####
+# Update the queue to reflect the fact that Patricia has talked her way 
+# in front of Steve with just one item.
 (queue <- queue[c(1, 4, 2, 3, 5)])
+# [1] "Mary"     "Patricia" "Steve"    "Alex"     "Harold" 
 
-# 1.e. Update the queue to reflect that fact that Harold has grown impatient
-#      and left.
+#### 1.e. ####
+# Update the queue to reflect that fact that Harold has grown impatient
+# and left.
 (queue <- queue[1:length(queue) - 1])
+# [1] "Mary"     "Patricia" "Steve"    "Alex" 
 
-# 1.f. Update the queue to reflect the fact that Alex has grown impatient and left.
-#      Do this as if  you do not know what slot Alex currently occupies by number.
-#      http://stat.ethz.ch/R-manual/R-patched/library/base/html/subset.html
+#### 1.f. ####
+# Update the queue to reflect the fact that Alex has grown impatient and left.
+# Do this as if  you do not know what slot Alex currently occupies by number.
+# http://stat.ethz.ch/R-manual/R-patched/library/base/html/subset.html
 notAlex <- queue != "Alex"
 (queue <- subset(queue, notAlex))
+# [1] "Mary"     "Patricia" "Steve"  
 
-# 1.g. Identify the position of Patricia in the queue
-#      http://www.r-tutor.com/r-introduction/vector/named-vector-members
+#### 1.g. ####
+# Identify the position of Patricia in the queue
+# http://www.r-tutor.com/r-introduction/vector/named-vector-members
 ndx <- c(1:length(queue))
 names(ndx) <- queue
 (ndx["Patricia"])
+# Patricia 
+#        2 
 
-# 1.h. Count the number of people in the queue
+#### 1.h. ####
+# Count the number of people in the queue
 (length(queue))
+# [1] 3
 
-######
-# 2. Modify your answer to quiz exercise 21 so that when you implement the 
-#    quadratic equation, meaningful outp is given whether there are one, two, or no solutons.
+#### 2. ####
+# Modify your answer to quiz exercise 21 so that when you implement the 
+# quadratic equation, meaningful outp is given whether there are one, two, or no solutons.
 a <- 2
 b <- 9 # b^2 = 81
 c <- 4
@@ -69,10 +86,11 @@ if(discrimant >= 0)
 
 # Show final result
 x
+# [1] -0.5 -4.0
 
-############
-# 3. Use R to determine how many numbers from 1 to 1000 are not divisible by any of 3,7, and 11
-#    http://markmail.org/message/dfmmyfjhsztqiake
+#### 3. ####
+# Use R to determine how many numbers from 1 to 1000 are not divisible by any of 3,7, and 11
+# http://markmail.org/message/dfmmyfjhsztqiake
 ds <- c(1:1000)
 three <- ds %% 3
 seven <- ds %% 7
@@ -85,11 +103,11 @@ elevenBit <- eleven != 0
 notDivisible <- ifelse(threeBit == 1 & sevenBit == 1 & elevenBit == 1, TRUE, FALSE)
 (countNotDivByAny <- sum(notDivisible)) # Ans: 520
 
-############
-# 4. Write R code that takes three input constants f, g, and h and determinues 
-#    whether they form a Pythagorean Triple (such that the square of the largest
-#    is equal to the sum of the squares of the other two constants)
-#    http://stat.ethz.ch/R-manual/R-patched/library/base/html/sort.html
+#### 4. ####
+# Write R code that takes three input constants f, g, and h and determinues 
+# whether they form a Pythagorean Triple (such that the square of the largest
+# is equal to the sum of the squares of the other two constants)
+# http://stat.ethz.ch/R-manual/R-patched/library/base/html/sort.html
 f <- 8
 g <- 6
 h <- 10
@@ -97,3 +115,4 @@ triple <- c(f, g, h)
 triple <- sort(triple, decreasing = TRUE)
 (tripleSquared <- triple^2)
 (isPythagTriple <- tripleSquared[1] == (tripleSquared[2] + tripleSquared[3]))
+# [1] TRUE
