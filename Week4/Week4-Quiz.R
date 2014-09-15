c0fe9ae24ef63e5332b088a56515135f848345c7
Daniel Dittenhafer <daniel@dittenhafer.net>
2014-09-15 14:02:24 -0400

    +3

--- Week4/Week4-Quiz.R
+++ Week4/Week4-Quiz.R
@@ -9,7 +9,7 @@ require(ggplot2)
 require(ggthemes)
 
 # Read in the data 
-movies <- read.table("C:/SourceCode/R/DataAcqMgmt/MovieData/movies.tab", sep="\t", header=TRUE, quote="", comment="")
+movies <- read.table("C:/Code/R/DataAcqMgmt/MovieData/movies.tab", sep="\t", header=TRUE, quote="", comment="")
 
 #### 1. ####
 # Show an appropriate visualization that displays the total number of movies for each decade.
@@ -29,7 +29,11 @@ g1
 #### 2. ####
 # Show the average IMBD user rating for different genres of movies. Has this changed over time?
 # 
-# Answer: Yes, ratings have changed over time. 
+# Answer: Yes, ratings have changed over time. Based on the faceted chart 
+#         produced by the following code, each genre has had it's own 
+#         fluctuations through the decades, though all genres generally
+#         saw a signficant increase in average rating during the 1900 and 
+#         1910 decades.
 #
 # First, helper function for determining average rating per genre for a set of movies.
 get.avgRating <- function(genres, someMovies)
@@ -63,10 +67,50 @@ for (d in decades)
 # Eliminate NAs
 ratingOverTime <- subset(ratingOverTime, !is.na(ratingOverTime$genre))
 
-# Build faceted chart by genra showing how ratings have changed over time.
+# Build faceted chart by genre showing how ratings have changed over time.
 g2 <- ggplot(data=ratingOverTime, aes(x=decade, y=avg.rating))
 g2 <- g2 + geom_line(aes(group=1))
 g2 <- g2 + facet_wrap(~ genre)
 g2 <- g2 + theme(axis.text.x = element_text(angle=45, vjust=1))
 g2 <- g2 + labs(title="Average User Ratings for Movies by Genre per Decade", x="Decade", y="Average User Rating")
 g2
+
+#### 3. ####
+# Is there a relationship between length of movie and movie rating?
+#
+# Answer: No, length of movie and rating has no relationship.
+#
+# First, use mean + 2 stdev as guide to remove outliers
+upperThreshold <- (mean(movies$length) + ( 2 * sd(movies$length)))
+print(upperThreshold)
+moviesNoLengthOutliers <- subset(movies, movies$length <= upperThreshold)
+
+# Build scatter plot for rating by length of movie
+g3 <- ggplot(data=moviesNoLengthOutliers, aes(x=length, y=rating))
+g3 <- g3 + geom_point()
+g3 <- g3 + labs(title="Movie Rating by Length", x="Length", y="Average User Rating")
+g3
+
+# Run linear regression to see that it says
+movies3 <- cbind(movies, ones=c(rep(1, nrow(movies))))
+nOnesRating <- as.matrix(movies3[,c("ones", "length")])
+#print(mode(nOnesRating))
+#print(class(nOnesRating))
+X <- as.matrix(nOnesRating)
+y <- as.matrix(movies3[,"rating"])
+
+(B.hat <- solve( t(X) %*% X ) %*% t(X) %*% y  )  # solve takes the inverse
+print(B.hat)
+# ones    6.021470578
+# length -0.001076301
+#
+# Properly, we should do an hypothesis test on the Betas to determine if we can accept
+# the alternate hypothesis that B1 is != 0... but for now, we will take the value at 
+# its face value.
+
+# y.hat <- X %*% B.hat
+# print(head(y.hat))
+# movies4 <- cbind(movies3, y.hat)
+# 
+# SE <- sqrt( sum ( (movies$rating - movies$y.hat) ^ 2) / length(movies) - 2) / sqrt( sum( (movies$length - mean(movies$length)) ^ 2))
+# print(SE)
