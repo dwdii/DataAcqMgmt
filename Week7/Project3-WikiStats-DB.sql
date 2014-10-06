e068bafa2dbcc83e285ffb681567486ba22200e8
Dan Dittenhafer <daniel@dittenhafer.net>
2014-10-05 22:42:48 -0400

    Prj3 Start

---
+++ Week7/Project3-WikiStats-DB.sql
@@ -0,0 +1,30 @@
+﻿-- 1. Create a new database for the wiki stats data.
+-- CREATE DATABASE wikidata;
+DROP TABLE pageviews;
+DROP TABLE page;
+DROP TABLE language;
+
+CREATE TABLE language (
+	id serial PRIMARY KEY,
+	languagecode VARCHAR(6) UNIQUE
+);
+
+CREATE TABLE page (
+	id serial PRIMARY KEY,
+	page text,
+	languageid INT,
+
+	CONSTRAINT Page_Language FOREIGN KEY (languageid) REFERENCES language(id)
+);
+
+CREATE TABLE pageviews (
+	id serial PRIMARY KEY,
+	hourstamp timestamp without time zone, 
+	pageid INT,
+	contentsize numeric(18,0),
+	
+	UNIQUE (hourstamp, pageid),
+	CONSTRAINT PageView_Page FOREIGN KEY (pageid) REFERENCES page(id)
+
+);
+
