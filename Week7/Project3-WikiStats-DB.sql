9ab47ae926fec69f93df5d30c60326cf3d278dc0
Dan Dittenhafer <daniel@dittenhafer.net>
2014-10-06 21:50:06 -0400

    Proj3

--- Week7/Project3-WikiStats-DB.sql
+++ Week7/Project3-WikiStats-DB.sql
@@ -11,7 +11,7 @@ CREATE TABLE language (
 
 CREATE TABLE page (
 	id serial PRIMARY KEY,
-	page text,
+	page text UNIQUE,
 	languageid INT,
 
 	CONSTRAINT Page_Language FOREIGN KEY (languageid) REFERENCES language(id)
@@ -21,6 +21,7 @@ CREATE TABLE pageviews (
 	id serial PRIMARY KEY,
 	hourstamp timestamp without time zone, 
 	pageid INT,
+	pageviews INT,
 	contentsize numeric(18,0),
 	
 	UNIQUE (hourstamp, pageid),
@@ -28,7 +29,19 @@ CREATE TABLE pageviews (
 
 );
 
+/*
+SELECT * FROM rawstage ORDER BY pageviews DESC LIMIT 10;
 
-SELECT * FROM rawstage ORDER BY pageviews DESC LIMIT 10
+SELECT * FROM language;
+SELECT * FROM page LIMIT 100;
 
-SELECT * FROM language
+SELECT * FROM pageviews LIMIT 100
+
+SELECT * FROM rawstage ORDER BY pageviews DESC LIMIT 10;
+
+SELECT p.page, pv.pageviews FROM page p
+	LEFT JOIN pageviews pv ON pv.pageid = p.id
+	ORDER BY pv.pageviews DESC
+	LIMIT 10;
+	
+*/
--- Week7/Project3-WikiStats-ETL.R
+++ Week7/Project3-WikiStats-ETL.R
@@ -8,22 +8,30 @@
 require("RPostgreSQL")
 
 # Load the file into a data.frame
-path <- file.path("C:/Code/R/DataAcqMgmt/Week7/Data", "20141001140000.txt")
-wiki.data <- read.table(path, header = FALSE, sep=" ")
-colnames(wiki.data) <- c("language.proj", "page", "pageviews", "contentSize")
+path <- file.path("C:/SourceCode/R/DataAcqMgmt/Week7/Data", "20141001140000.txt")
+hourstamp = '2014-10-01 14:00:00'
+wiki.data <- read.table(path, header = FALSE, sep=" ", stringsAsFactors=FALSE)
+colnames(wiki.data) <- c("language", "page", "pageviews", "contentsize")
 
 # Show me
 head(wiki.data)
-
 summary(wiki.data)
 
-languages <- levels(as.factor(wiki.data$language.proj))
+#wiki.data2 <- tidyr::separate(wiki.data, language.proj, into = c("language", "project"), sep="[^a-z\\.]", convert=FALSE, remove=FALSE, fixed=TRUE)
+#head(wiki.data2)
+wiki.data2 <- wiki.data
+#colnames(wiki.data2) <- c("language", "page", "pageview")
 
-# PostgreSQL Version: 9.3.5.1
+# Get unique languages
+languages <- levels(as.factor(wiki.data2$language))
+wiki.data3 <- cbind(wiki.data2, langid=NA)
+
+# PostgreSQL - connect
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv,user="postgres",password="a",dbname="wikidata")
 
 # Insert the languages in the language table.
+langIds <- data.frame(language=NULL, id=NULL)
 for (l in languages)
 {
   sQuery <- sprintf("SELECT id FROM language WHERE languagecode = '%s'", l)
@@ -38,15 +46,42 @@ for (l in languages)
   else
   {
     sInsert <- sprintf("INSERT INTO language (languagecode) VALUES ('%s') RETURNING id", l)
+    print (sInsert)
     res <- dbGetQuery(con, sInsert)
+    
   }
+
+  # Keep the lang Ids and update our data.frame accordingly.
+  langIds <- rbind(langIds, language = l, id = res[1,1])
+  wiki.data3[wiki.data3$language == l,]$langid <- res[1,1]
 }
 
+# Show me
+head(langIds)
+head(wiki.data3)
+
+# Bulk insert the raw data into a staging table.
 if(dbExistsTable(con, "rawstage")){
   dbRemoveTable(con, "rawstage")
 }
+dbWriteTable(con, "rawstage", wiki.data3)
 
-dbWriteTable(con, "rawstage", wiki.data)
+# Now swap the page data into the page table
+sQuery <- sprintf("INSERT INTO page (page, languageid) SELECT page, langid FROM rawstage")
+print (sQuery)
+res <- dbGetQuery(con, sQuery)
 
+# Finally swap the pageviews data into the pageviews table
+sQuery <- sprintf("INSERT INTO pageviews (hourstamp, pageid, pageviews, contentsize) SELECT '%s', p.id, r.pageviews, r.contentsize FROM rawstage r INNER JOIN page p ON p.page = r.page", hourstamp)
+#sQuery <- sprintf("SELECT '%s', 1, pageviews, contentsize FROM rawstage r", hourstamp)
+print (sQuery)
+res <- dbGetQuery(con, sQuery)
+
+# 
+sQuery <- "SELECT p.page, pv.pageviews FROM page p LEFT JOIN pageviews pv ON pv.pageid = p.id ORDER BY pv.pageviews DESC LIMIT 10"
+print (sQuery)
+res <- dbGetQuery(con, sQuery)
+
+head(res)
 
 
