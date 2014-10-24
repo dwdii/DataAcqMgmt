87dee41cfdbfac265133f99c61776585b0258b9f
Daniel Dittenhafer <daniel@dittenhafer.net>
2014-10-23 22:37:42 -0400

    initial hw p2

---
+++ Week9/Week9-Hw.R
@@ -0,0 +1,61 @@
+# 
+# Author: Daniel Dittenhafer
+#
+# Created: Oct 23, 2014
+#
+# Description: Answers to Week 9 Assignment Part 2
+#
+require (rmongodb)
+
+# Our MongoDB Connection Settings
+mongohost <- "localhost:27017"
+user <- ""
+pass <- ""
+db <- "unitedstates"
+authdb <- db
+ns = sprintf("%s.%s", db, "localities")
+
+# Connect to Mongo.
+mongo <- mongo.create(host=mongohost, username=user, password=pass, db=authdb)
+print (mongo)
+
+# If we got an error connecting, report it...
+if(!mongo.is.connected(mongo)) {
+  print(mongo.get.last.err(mongo))
+} else {
+  # Get the Territories data frame
+  query <- "{ \"territory\": { \"$exists\": \"true\" }}"
+  cursor <- mongo.find(mongo, ns, query)
+  dfTerritories <- mongo.cursor.to.data.frame(cursor)
+  
+  head(dfTerritories)
+  
+  # Get the States data frame
+  query <- "{ \"state\": { \"$exists\": \"true\" }}"
+  cursor <- mongo.find(mongo, ns, query)
+  dfStates <- mongo.cursor.to.data.frame(cursor)
+  
+  head(dfStates)  
+  
+    # Get the Federal Districts data frame
+  query <- "{ \"federal_district\": { \"$exists\": \"true\" }}"
+  cursor <- mongo.find(mongo, ns, query)
+  dfDistricts <- mongo.cursor.to.data.frame(cursor)
+  
+  head(dfDistricts)    
+  
+  # Clean up dates to be Date data type.
+  dfDistricts <- data.frame(dfDistricts, establishmentDate=as.Date(dfDistricts$establishment_date, "%m/%d/%Y"))
+  dfStates <- data.frame(dfStates, statehoodDate=as.Date(dfStates$statehood_date, "%m/%d/%Y"))
+  
+  # Clean up extra quote marks and split the territorial status into two columns
+  dfTerritories$territorial_status <- stringr::str_replace_all(dfTerritories$territorial_status, "\"", "")
+  dfTerritoriesTidy <- tidyr::separate(dfTerritories, "territorial_status", c("is_incorporated", "is_organized"), sep=", ")
+
+  # Show the clead data frames
+  head(dfStates)  
+  head(dfDistricts)  
+  head(dfTerritoriesTidy)
+}
+
+
