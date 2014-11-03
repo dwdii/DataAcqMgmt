3a3c795d17a53caccbb0f66bca4abfebe1644e99
Daniel Dittenhafer <daniel@dittenhafer.net>
2014-11-03 12:45:58 -0500

    Part 4 start

---
+++ Week10/Project4-Mongo-DataImp.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongoimport.exe" --host localhost --db proj4 -c fmr2015 --file C:\Code\R\DataAcqMgmt\Week10\Data\FY2015F_4050_Final-utf8.csv --headerline --type csv
+PAUSE
