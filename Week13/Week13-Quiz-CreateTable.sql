03361a9de065ba13051b75e3ec859e9a394ef84c
Daniel Dittenhafer <daniel@dittenhafer.net>
2014-11-19 12:34:10 -0500

    complete

--- Week13/Week13-Quiz-CreateTable.sql
+++ Week13/Week13-Quiz-CreateTable.sql
@@ -8,5 +8,8 @@
 SELECT upsert_description('Jeep', 'maroon');
 SELECT upsert_description('Prius', 'gold');
 SELECT upsert_description('Odyssey', 'blue');
+SELECT upsert_description('Prius', 'silver');
 
 SELECT * FROM ObjectList;
+
+--DELETE FROM ObjectList
